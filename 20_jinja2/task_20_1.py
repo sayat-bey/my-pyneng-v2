ed69ae511dcc07bc9bb4933de77e1d44b8c9e16a
sayat-bey <sayat.bey@ya.ru>
2020-11-15 00:07:59 +0600

    20.1-5

---
+++ 20_jinja2/task_20_1.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Задание 20.1
+
+Создать функцию generate_config.
+
+Параметры функции:
+* template - путь к файлу с шаблоном (например, "templates/for.txt")
+* data_dict - словарь со значениями, которые надо подставить в шаблон
+
+Функция должна возвращать строку с конфигурацией, которая была сгенерирована.
+
+Проверить работу функции на шаблоне templates/for.txt и данных из файла data_files/for.yml.
+
+"""
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+
+def generate_config(template, data_dict):
+    
+    template_dir, template_file = template.split("/")
+
+    env = Environment(loader=FileSystemLoader(template_dir), trim_blocks=True, lstrip_blocks=True)
+    t = env.get_template(template_file)
+    
+    return t.render(data_dict)
+
+# так должен выглядеть вызов функции
+if __name__ == "__main__":
+    data_file = "data_files/for.yml"
+    template_file = "templates/for.txt"
+    with open(data_file) as f:
+        data = yaml.safe_load(f)
+    print(generate_config(template_file, data))
